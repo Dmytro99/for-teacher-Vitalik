17981f584cbd8daddd8ef90ab1a03937e2d03481
Dmytro99 <demkovychdmytro@gmail.com>
2020-04-17 10:04:43 +0300

    CROS fix added.

--- springBootCrud/src/main/java/com/dmytryk/crud/controller/UserController.java
+++ springBootCrud/src/main/java/com/dmytryk/crud/controller/UserController.java
@@ -13,12 +13,13 @@ import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
